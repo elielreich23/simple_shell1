f2cbde0087c7e8776fca7b780f38eef4ad927b8e
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2022-11-14 08:58:47 -0300

    Update builtin_funcs.c

--- builtin_funcs.c
+++ builtin_funcs.c
@@ -1,3 +1,23 @@
+#include "shell.h"
+
+/**
+ *env - prints the current_environnement
+ *@tokenized_command: command entered
+ *
+ *Return: void
+ */
+
+void env(char **tokenized_command __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
+	}
+}
+
 /**
  * quit - exits the shell
  * @tokenized_command: command entered
