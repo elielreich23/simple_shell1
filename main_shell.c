4c3bbce86a1814a7266be09a0fb7054be7cee03b
elielreich23 <elielreich@yahoo.com>
2022-11-17 18:42:31 -0300

    update

--- main_shell.c
+++ main_shell.c
@@ -1,11 +1,6 @@
 #include "shell.h"
 
 
-	char **commands = NULL;
-	char *line = NULL;
-	char *shell_name = NULL;
-	int status = 0;
-
 /**
  * main - the main shell code
  * @argc: number of arguments passed
@@ -15,7 +10,11 @@
  * implements EOF
  * Prints error on Failure
  *Return: 0 on success
- */
+ *char **commands = NULL;
+ *char *line = NULL;
+ *char *shell_name = NULL;
+ *int status = 0;
+*/
 
 
 int main(int argc __attribute__((unused)), char **argv)
